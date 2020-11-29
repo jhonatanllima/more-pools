0a72dcf925253e54fce7f250723a215a2d7bcfd0
Jhonatan Lima <39390551+jhonyllima@users.noreply.github.com>
2020-11-29 18:26:28 -0300

    Added Colors in Themes and adjust Font family in Global

--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -1,14 +1,16 @@
 import styled from 'styled-components'
 
+import colors from '../../themes/colors'
+
 export const Content = styled.nav`
   width: 100%;
-  height: 60px;
+  height: 50px;
   display: flex;
   padding: 5px;
   align-items: center;
   justify-content: space-around;
 
-  background: #e7e7e7;
+  background: ${colors.darkWhite};
 
   .logo img {
     width: 100%;
@@ -29,12 +31,11 @@ export const Content = styled.nav`
   }
 
   .nav-list .nav-list-link a {
-    color: #777;
-    cursor: pointer;
+    color: ${colors.textGrey};
     font-size: 16px;
     transition: 0.5s;
+    cursor: pointer;
     font-weight: 500;
-    font-family: Arial, Helvetica, sans-serif;
     text-decoration: none;
   }
 
--- src/themes/global.ts
+++ src/themes/global.ts
@@ -8,6 +8,6 @@ export default createGlobalStyle`
   }
 
   body {
-    font-family: Arial, Helvetica, sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 `
