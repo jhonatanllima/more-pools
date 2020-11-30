a51a1a63b4bc5e31bb4bb7d1ca7cfc040fd65ff4
vitoremanuelqf <vitoremanuelqf@gmail.com>
2020-11-30 02:07:18 -0300

    created responsiveness in navbar

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,24 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { FaBars } from 'react-icons/fa'
 import logoImg from '../../assets/images/logo.png'
 
-import {
-  Container,
-  ContentWrapper,
-  List,
-  Logo,
-  Container,
-  Content,
-  Logo
-} from './styles'
+import { Container, ContentWrapper, List, Logo } from './styles'
 import 'animate.css'
 
 const Nav: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
   return (
     <Container>
       <ContentWrapper>
-        <Logo>
+        <Logo className="logo">
           <img
             src={logoImg}
             alt="logo"
@@ -26,7 +20,12 @@ const Nav: React.FC = () => {
           />
         </Logo>
 
-        <List>
+        <List
+          className="list"
+          style={{
+            transform: openMenu ? 'translateX(0px)' : ''
+          }}
+        >
           <ul>
             <li>Empresa</li>
             <li>Cátalogo</li>
@@ -35,101 +34,10 @@ const Nav: React.FC = () => {
             <li>Contato</li>
           </ul>
         </List>
-        <FaBars />
+        <FaBars onClick={() => setOpenMenu(!openMenu)} className="burguer" />
       </ContentWrapper>
     </Container>
   )
 }
 
 export default Nav
-
-const Nav: React.FC = () => {
-  return (
-    <Container>
-      <Content>
-        <Logo>
-          <img
-            src={logoImg}
-            alt="logo"
-            className="animate__animated animate__bounceInDown"
-          />
-        </Logo>
-
-        <ul className="nav-list">
-          <li className="nav-list-link">
-            <a href="teste">Empresa</a>
-          </li>
-          <li className="nav-list-link">
-            <a href="teste">Catálogo</a>
-          </li>
-          <li className="nav-list-link">
-            <a href="teste">Promoções</a>
-          </li>
-          <li className="nav-list-link">
-            <a href="teste">Galeria</a>
-          </li>
-          <li className="nav-list-link">
-            <a href="teste">Contato</a>
-          </li>
-        </ul>
-      </Content>
-    </Container>
-  )
-}
-
-export default Nav
-
-// ------------------------------
-
-// import styled from 'styled-components'
-
-// import colors from '../../themes/colors'
-
-// export const Container = styled.div`
-//   width: 100%;
-// `
-// export const Content = styled.nav`
-//   width: 100%;
-//   height: 50px;
-//   display: flex;
-//   padding: 5px;
-//   align-items: center;
-//   justify-content: space-around;
-
-//   background: ${colors.darkWhite};
-
-//   .nav-list {
-//     display: flex;
-//     justify-content: space-around;
-
-//     list-style-type: none;
-//   }
-
-//   .nav-list .nav-list-link {
-//     margin-left: 20px;
-//   }
-
-//   .nav-list .nav-list-link a {
-//     color: ${colors.textGrey};
-//     font-size: 16px;
-//     transition: 0.5s;
-//     cursor: pointer;
-//     font-weight: 500;
-//     text-decoration: none;
-//   }
-
-//   .nav-list .nav-list-link a:hover {
-//     color: #333;
-//   }
-// `
-// export const Logo = styled.div`
-//   width: auto;
-//   height: auto;
-
-//   img {
-//     width: 100%;
-//     height: auto;
-//     max-width: 120px;
-//     margin-bottom: -40px;
-//   }
-// `
--- src/components/Nav/styles.ts
+++ src/components/Nav/styles.ts
@@ -11,24 +11,56 @@ export const ContentWrapper = styled.nav`
   width: 100%;
   height: 100%;
   display: flex;
-  padding: 0 10vw;
+  padding: 0 4vw;
   align-items: center;
   justify-content: space-between;
 
-  background-color: ${colors.darkWhite};
+  background: ${colors.darkWhite};
+
+  .burguer {
+    display: none;
+  }
+
+  @media (max-width: 768px) {
+    .burguer {
+      right: 5%;
+      position: absolute;
+
+      display: block;
+
+      font-size: 1.5rem;
+    }
+
+    .list {
+      top: 0;
+      left: 0;
+      position: absolute;
+
+      width: 50vw;
+      height: 100vh;
+      display: flex;
+      padding: 24px 24px 24px 0;
+      transform: translateX(-50vw);
+      transition: 0.5s ease-in-out;
+      align-items: flex-start;
+      justify-content: start;
+
+      background: ${colors.darkWhite};
+    }
+  }
 `
 
 export const Logo = styled.div`
   width: auto;
   height: 100%;
   display: flex;
+  padding: 5px;
   align-items: center;
   justify-content: center;
 
   img {
-    width: 100%;
-    height: auto;
-    max-width: 100px;
+    height: 100%;
+    max-height: 60px;
   }
 `
 
@@ -37,12 +69,32 @@ export const List = styled.div`
 
   ul {
     display: flex;
-    justify-content: space-around;
 
     list-style-type: none;
   }
 
   ul li {
-    margin-left: 15px;
+    margin-left: 25px;
+  }
+
+  ul li a {
+    cursor: pointer;
+  }
+
+  @media (max-width: 768px) {
+    ul {
+      width: 100%;
+      margin: 0;
+      display: block;
+    }
+
+    ul li {
+      padding: 10px 0;
+      border-bottom: 1px solid #fff;
+    }
+
+    ul li:last-of-type {
+      border-bottom: 0px solid #fff;
+    }
   }
 `
