a997c95aa35f1b35647d2cf887c0d202aa21db5a
vitoremanuelqf <vitoremanuelqf@gmail.com>
2020-12-01 19:56:29 -0300

    change text in promotion

--- src/components/Highlights/index.tsx
+++ src/components/Highlights/index.tsx
@@ -1,28 +1,9 @@
 import React from 'react'
 
-<<<<<<< HEAD
-import Card from '../CardProducts/index'
-
-import { Container, ContentWrapper, ContentRight, ContentLeft } from './styles'
+// import { Container } from './styles';
 
 const Highlights: React.FC = () => {
-  return (
-    <Container>
-      <ContentWrapper>
-        <ContentLeft>
-          <h1>Promoções</h1>
-        </ContentLeft>
-
-        <ContentRight>
-          <Card />
-        </ContentRight>
-      </ContentWrapper>
-    </Container>
-  )
-=======
-const Promotion: React.FC = () => {
   return <div />
->>>>>>> e27b9f64378e8a87cb832987d26255b63227f2a0
 }
 
-export default Promotion
+export default Highlights
